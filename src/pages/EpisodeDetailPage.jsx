934a31e2a25b54f3bea520381fe50699fd6a6334
“gor-vika” <buryak154@gmail.com>
2024-04-07 23:28:57 +0300

    edit

--- src/pages/EpisodeDetailPage.jsx
+++ src/pages/EpisodeDetailPage.jsx
@@ -1,6 +1,4 @@
 import HeroSection from '../components/common/HeroSection'
-import Episodes from '../components/Episodes'
-import SectionEpisodeFull from '../components/SectionEpisodeFull'
 
 export default function EpisodeDetailPage(){
 
